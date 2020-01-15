08588c98267bb50587b080c5fa884f44046a7e74
Papa <pawuahmantey@gmail.com>
2020-01-15 18:19:51 -0500

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Papa Kwaku  on 1/15/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ontTap(_ sender: Any) {
+        
+        print("Hello")
+        view.endEditing(true)
+    }
+ 
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        
+    }
+}
+
